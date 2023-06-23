576e9a6fa2e477ce0b9b38da45f09896e91a4c18
Rohan Verma <86923317+DevOpRohan@users.noreply.github.com>
2023-06-24 01:04:39 +0530

    update db_string

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://Git:289Bh6Wj473HDEA0@recipe-book-db-69fd315b.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=recipe-book-db', {
+    await mongoose.connect('mongodb+srv://Dev:Dvq785LM01wjV942@recipe-book-db-69fd315b.mongo.ondigitalocean.com/admin?tls=true&authSource=admin&replicaSet=recipe-book-db', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
